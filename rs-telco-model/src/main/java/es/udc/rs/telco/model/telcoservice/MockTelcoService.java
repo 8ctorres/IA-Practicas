3bb7e5a0985144828b35dfdd7c9218dc551a28c5
Carlos Torres <carlos.torres@udc.es>
2021-09-16 23:03:30 +0200

    [MOD-MAKBILL] Primeira versión da operación 3 da capa Modelo
    Falta facer o caso de cambiar de "BILLED" a "PAID"
    Falta implementar unhas excepcións de xeito para os erros. Por ahora para que non dea erros está con RuntimeException
    Falta facer e pasar os tests de unidade

--- rs-telco-model/src/main/java/es/udc/rs/telco/model/telcoservice/MockTelcoService.java
+++ rs-telco-model/src/main/java/es/udc/rs/telco/model/telcoservice/MockTelcoService.java
@@ -1,17 +1,22 @@
 package es.udc.rs.telco.model.telcoservice;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 import es.udc.rs.telco.model.customer.Customer;
 import es.udc.rs.telco.model.phonecall.PhoneCall;
+import es.udc.rs.telco.model.phonecall.PhoneCallStatus;
 
 public class MockTelcoService implements TelcoService {
 
 	private static Map<Long, Customer> clientsMap = new LinkedHashMap<Long, Customer>();
-	private static Map<Long,PhoneCall> phoneCallsMap = new LinkedHashMap<Long,PhoneCall>();
-	private static Map<Long,List<PhoneCall>> phoneCallsByUserMap = new LinkedHashMap<Long,List<PhoneCall>>();
+	private static Map<Long, PhoneCall> phoneCallsMap = new LinkedHashMap<Long,PhoneCall>();
+	//private static Map<Long, List<PhoneCall>> phoneCallsByUserMap = new LinkedHashMap<Long,List<PhoneCall>>();
 
 	private static long lastClientId = 0;
 	private static long lastPhoneCallId = 0;
@@ -23,6 +28,39 @@ public class MockTelcoService implements TelcoService {
 	
 	private static synchronized long getNextPhoneCallId() {
 		return ++lastPhoneCallId;
+	}
+
+	public static void getCallsbyMonth(Long customerId, int month, int year){
+		// Primeiro comprobamos que o mes que nos piden xa pasou
+		if (LocalDateTime.now().isBefore(LocalDateTime.of(year, month, 1, 0, 0).plusMonths(1))){
+			// TODO: Poñer unha excepción axeitada para este erro
+			throw new RuntimeException("O mes aínda non rematou");
+		}
+
+		List<PhoneCall> calls = new ArrayList<>();
+		for (PhoneCall call: phoneCallsMap.values()) {
+			// Recorremos toda a colección de chamadas
+			if (call.getCustomerId() == customerId){
+				// Si é do cliente que esperamos, comprobamos o mes e o ano
+				LocalDateTime date = call.getStartDate();
+				if ((date.getMonthValue() == month) && (date.getYear() == year)){
+					// Si coincide, gardamos o obxeto PhoneCall nunha nova lista
+					calls.add(call);
+				}
+			}
+		}
+		// Agora percorremos toda a lista de chamadas e comprobamos que todas están en estado "PENDING"
+		for (PhoneCall call: calls) {
+			if (!(call.getPhoneCallStatus().equals(PhoneCallStatus.PENDING))){
+				// Si atopamos algunha que non estea PENDING, sacamos un erro
+				// TODO: Poñer unha excepción axeitada para este erro
+				throw new RuntimeException("Hai unha chamada que non está en estado PENDING");
+			}
+		}
+		// Agora volvemos a percorrer a lista de chamadas e poñémolas todas a estado "BILLED"
+		for (PhoneCall call: calls) {
+			call.setPhoneCallStatus(PhoneCallStatus.BILLED);
+		}
 	}
 	
 }
