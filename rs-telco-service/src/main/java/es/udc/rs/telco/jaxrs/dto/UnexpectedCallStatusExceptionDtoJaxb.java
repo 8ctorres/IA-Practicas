3f403d1eac505bbd7a5cba765153f89a57af4319
IsmaVerde <ismael.verdec@udc.es>
2021-11-17 23:13:25 +0100

    [SRV-EXCEPT] Excepción de MonthNotClosed y excepción UnexpectedCallStatus añadidas

---
+++ rs-telco-service/src/main/java/es/udc/rs/telco/jaxrs/dto/UnexpectedCallStatusExceptionDtoJaxb.java
@@ -0,0 +1,46 @@
+package es.udc.rs.telco.jaxrs.dto;
+
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlElement;
+
+@XmlRootElement(name="unexpectedCallStatusException")
+@XmlType(name="unexpectedCallStatusExceptionType")
+public class UnexpectedCallStatusExceptionDtoJaxb {
+
+    @XmlAttribute(required = true)
+    private String errorType;
+    @XmlElement(required = true)
+    private String message;
+
+    public UnexpectedCallStatusExceptionDtoJaxb(String message) {
+        this.errorType = "UnexpectedCallStatus";
+        this.message = message;
+    }
+
+    public String getErrorType() {
+        return errorType;
+    }
+
+    public void setErrorType(String errorType) {
+        this.errorType = errorType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "UnexpectedCallStatusExceptionDtoJaxb{" +
+                "errorType='" + errorType + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+}
