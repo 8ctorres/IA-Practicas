9efb89cd978eb06f1509a31ea75de9dd31486b95
IsmaVerde <ismael.verdec@udc.es>
2021-11-17 17:36:36 +0100

    [SERV-CALLDTO] Añadido dto para PhoneCall

---
+++ rs-telco-service/src/main/java/es/udc/rs/telco/jaxrs/dto/PhoneCallDto.java
@@ -0,0 +1,97 @@
+package es.udc.rs.telco.jaxrs.dto;
+
+import es.udc.rs.telco.model.phonecall.PhoneCallStatus;
+import es.udc.rs.telco.model.phonecall.PhoneCallType;
+
+import java.time.LocalDateTime;
+
+public class PhoneCallDto {
+    private Long phoneCallId;
+    private Long customerId;
+    private LocalDateTime startDate;
+    private Long duration;
+    private String destinationNumber;
+    private PhoneCallType phoneCallType;
+    private PhoneCallStatus phoneCallStatus;
+
+    public PhoneCallDto(Long phoneCallId, Long customerId, LocalDateTime startDate,
+                        Long duration, String destinationNumber, PhoneCallType phoneCallType,
+                        PhoneCallStatus phoneCallStatus) {
+        this.phoneCallId = phoneCallId;
+        this.customerId = customerId;
+        this.startDate = startDate;
+        this.duration = duration;
+        this.destinationNumber = destinationNumber;
+        this.phoneCallType = phoneCallType;
+        this.phoneCallStatus = phoneCallStatus;
+    }
+
+    public Long getPhoneCallId() {
+        return phoneCallId;
+    }
+
+    public void setPhoneCallId(Long phoneCallId) {
+        this.phoneCallId = phoneCallId;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+
+    public String getDestinationNumber() {
+        return destinationNumber;
+    }
+
+    public void setDestinationNumber(String destinationNumber) {
+        this.destinationNumber = destinationNumber;
+    }
+
+    public PhoneCallType getPhoneCallType() {
+        return phoneCallType;
+    }
+
+    public void setPhoneCallType(PhoneCallType phoneCallType) {
+        this.phoneCallType = phoneCallType;
+    }
+
+    public PhoneCallStatus getPhoneCallStatus() {
+        return phoneCallStatus;
+    }
+
+    public void setPhoneCallStatus(PhoneCallStatus phoneCallStatus) {
+        this.phoneCallStatus = phoneCallStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "PhoneCallDto{" +
+                "phoneCallId=" + phoneCallId +
+                ", customerId=" + customerId +
+                ", startDate=" + startDate +
+                ", duration=" + duration +
+                ", destinationNumber='" + destinationNumber + '\'' +
+                ", phoneCallType=" + phoneCallType +
+                ", phoneCallStatus=" + phoneCallStatus +
+                '}';
+    }
+}
