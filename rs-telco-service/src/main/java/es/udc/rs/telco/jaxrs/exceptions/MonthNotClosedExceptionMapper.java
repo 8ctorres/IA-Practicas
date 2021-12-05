73cace2950f64c72aaf82a2d05ed280c319df536
pabloroade <pablo.roade@udc.es>
2021-12-05 20:12:02 +0100

    [SERVICE] Añadida excepción genérica

--- rs-telco-service/src/main/java/es/udc/rs/telco/jaxrs/exceptions/MonthNotClosedExceptionMapper.java
+++ rs-telco-service/src/main/java/es/udc/rs/telco/jaxrs/exceptions/MonthNotClosedExceptionMapper.java
@@ -1,19 +1,19 @@
 package es.udc.rs.telco.jaxrs.exceptions;
 
+import es.udc.rs.telco.jaxrs.dto.ApplicationExceptionDtoJaxb;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 import es.udc.rs.telco.model.exceptions.MonthNotClosedException;
-import es.udc.rs.telco.jaxrs.dto.MonthNotClosedExceptionDtoJaxb;
 
 @Provider
 public class MonthNotClosedExceptionMapper implements ExceptionMapper<MonthNotClosedException> {
 
     @Override
     public Response toResponse(MonthNotClosedException ex) {
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(new MonthNotClosedExceptionDtoJaxb(ex.getMessage()))
+        return Response.status(Response.Status.CONFLICT)
+                .entity(new ApplicationExceptionDtoJaxb("MonthNotClosed", ex.getMessage()))
                 .build();
     }
 }
